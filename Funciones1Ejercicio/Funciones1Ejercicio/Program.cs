1d214c2894eb291b959b221ab1d4c0fa86fbc82a
LocoMenXD <105610399+LocoMenXD@users.noreply.github.com>
2022-05-18 14:51:56 -0400

    Add files via upload

---
+++ Funciones1Ejercicio/Funciones1Ejercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Crea un método que nos salude, pasándole un nombre por parámetro.
+
+namespace Funciones1Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            saludar("ChicowowxD"); //Llamada del método
+            Console.ReadLine();
+        }
+
+        //Método que saluda
+        public static void saludar(string nombre)
+        {
+
+            Console.WriteLine("¡Hola " + nombre + " !");
+            // Otra forma de hacerlo (Comenta lña anterior linea)
+            //Console.WriteLine("¡Hola {0} !", nombre);
+
+        }
+
+    }
+
+}
---
+++ Funciones1Ejercicio/Funciones2Ejercicio/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Crea una función que sume dos números pasados por parámetros, devolverá el resultado.
+
+namespace Funciones2Ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado = suma(10, 5); //Llamada de la función, lo guardo en una variable
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+        }
+
+        //Función que nos devuelve la suma de dos números
+        public static int suma(int num1, int num2)
+        {
+
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+    }
+}
